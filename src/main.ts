4b28d628c6619c82e36e94c2cd68ba54d6215c54
Matheus Wells <36707321+Matheuswells@users.noreply.github.com>
2021-05-04 08:10:57 -0300

    swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const config = new DocumentBuilder()
+    .setTitle('Ubistart API')
+    .setDescription('API description')
+    .setVersion('0.0.1')
+    .addTag('ubistart')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
